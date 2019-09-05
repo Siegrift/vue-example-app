c8496b1c2886cc173b488e9ef949544e987ba546
Emanuel Tesar <siegrift@google.com>
2019-09-05 17:04:32 +0200

    Example app

--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 yarn install
 ```
 
+To use the trusted types aware vue library follow these steps.
+1. `git clone https://github.com/Siegrift/vue.git` (clone it anywhere)
+2. Build the trusted types aware vue `yarn build`
+3. Replace this application's **node_modules/vue/dist** with the **dist** directory from Vue _(Basically, will copy the build files you've just build)_
+
 ### Compiles and hot-reloads for development
 ```
 yarn run serve
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,14 +7,62 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
+    
+    <!-- v-html directive -->
+    <div v-html="rawVHtml"></div>
+    <div v-html="safeVHtml"></div>
+
+    <!-- html interpolation -->
+    <div>{{rawInterHtml}}</div>
+    <div>{{safeInterHtml}}</div>
+    
+    <!-- attribute interpolation -->
+    <iframe :srcdoc="rawSrcdocHtml"></iframe>
+    <iframe :srcdoc="safeSrcdocHtml"></iframe>
+
+    <!-- on* events -->
+    <div>
+      <button :onclick="rawOnHandler">insecure btn</button>
+      <button :onclick="safeOnHandler">secure btn</button>
+    </div>
+
+    <!-- script tags causes compile errors -->
+    <!-- <script>{{scriptBody}}</script> -->
+
+    <!-- set and remove inner html -->
+    <div>
+      <span v-html="hideInnerHtml ? undefined: conditionalSafeVHtml"></span>
+      <button v-on:click="hideInnerHtml = !hideInnerHtml">Toggle innerHTML!</button>
+    </div>
   </div>
 </template>
 
 <script>
+const p = TrustedTypes.createPolicy('vue', {createHTML: (s) => s, createURL: (s) => s, createScript: (s) => s});
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: () => {
+    return {
+      rawVHtml: '<b>unsafe v-html</b>',
+      safeVHtml: p.createHTML('safe v-html'),
+      rawInterHtml: '<b>unsafe interpolated html</b>',
+      safeInterHtml: p.createHTML('safe interpolated html'),
+      rawSrcdocHtml: '<b>unsafe srcdoc html</b>',
+      safeSrcdocHtml: p.createHTML('safe srcdoc html'),
+      hideInnerHtml: false,
+      rawOnHandler: "console.log('inscure onclick')",
+      safeOnHandler: p.createScript("console.log('secure onclick')"),
+      scriptBody: 'console.log("script body")',
+      conditionalSafeVHtml: p.createHTML('safe condit html'),
+    }
+  },
+  render(c) {
+    console.log('whatever')
+  	return <div>asdaslndlsadnlsa</div>
+    // return c('span', {domProps: {x: undefined}, attrs: {id: 'test'}}, 'hello')
   }
 }
 </script>
